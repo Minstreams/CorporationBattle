53e0d2a9e8628ff1cad5823fed26d929eb02e550
Minsteams <474655650@qq.com>
2017-11-15 15:10:46 +0800

    增加了筛选功能

--- src/CostumeTerminal/CostumeSearch.java
+++ src/CostumeTerminal/CostumeSearch.java
@@ -9,16 +9,25 @@ import Model.Corporation;
  * 主界面状态
  */
 public class CostumeSearch extends State<CostumeLauncher> {
+    //是否为阻塞状态
+    private boolean blocked = false;
 
     //上一次键盘响应的时间
     private long timeMillis = 0;
+
+    /**
+     * 筛选模式
+     */
+    private enum FilterMode {
+        BeforeDate, AfterDate, ByCorporation
+    }
 
     @Override
     public void Enter(CostumeLauncher machine) {
         System.out.println("------------------进入主界面状态------------------");
         //输出表列
         machine.base.Output();
-        System.out.println("【输入社团或活动名查看详情！输入“进入后台”进入后台！】");
+        ShowTips();
         //初始化时间
         timeMillis = System.currentTimeMillis();
 
@@ -36,36 +45,126 @@ public class CostumeSearch extends State<CostumeLauncher> {
             machine.ChangeToState(new CostumePhotoWall());
         }
         //如果有键盘输入
-        if(InputStateMachine.getAnyKeyDown()){
+        if (InputStateMachine.getAnyKeyDown()) {
             //重置上次键盘响应时间
             timeMillis = time;
         }
+        //如果阻塞则直接返回
+        if (blocked)
+            return;
         //获取输入的字符串
-        String input =InputStateMachine.getInputString();
+        String input = InputStateMachine.getInputString();
         //如果输入不为空
-        if(!input.isEmpty()){
-            if(input.equals("进入后台")){
+        if (!input.isEmpty()) {
+            if (input.equals("进入后台")) {
                 //切换到后台状态
                 machine.ChangeToState(new AdministratorMode());
+                return;
+            }
+            if (input.equals("筛选")) {
+                //开始筛选
+                Filter(machine);
                 return;
             }
             //用一个社团变量，一个活动变量分别查找是否有相应的社团或活动
             Corporation cor = machine.base.FindCorporationWithName(input);
             Activity act = machine.base.FindActivityWithName(input);
             //如果有就输出
-            if(cor != null){
+            if (cor != null) {
                 cor.outputInformation();
-            }
-            else if(act != null){
+            } else if (act != null) {
                 act.outputInformation();
             }
             //否则显示没有找到相关信息
-            else{
+            else {
                 System.out.println("没有找到相关信息");
             }
             //结束给予提示
-            System.out.println("【输入社团或活动名查看详情！输入“进入后台”进入后台！】");
+            ShowTips();
         }
+    }
+
+    /**
+     * 开始筛选
+     */
+    private void Filter(CostumeLauncher machine) {
+        //开始阻塞
+        blocked = true;
+
+        FilterMode filterMode;
+        System.out.println("【输入“按社团”“日期前”“日期后”选择筛选模式】");
+        while (true) {
+            String input = InputStateMachine.readLine();
+            if (input.equals("按社团")) {
+                filterMode = FilterMode.ByCorporation;
+                System.out.println("【请输入社团名】");
+                break;
+            }
+            if (input.equals("日期前")) {
+                filterMode = FilterMode.BeforeDate;
+                System.out.println("【请输入日期：xxxx-xx-xx】");
+                break;
+            }
+            if (input.equals("日期后")) {
+                filterMode = FilterMode.AfterDate;
+                System.out.println("【请输入日期：xxxx-xx-xx】");
+                break;
+            }
+            System.out.println("无效指令！");
+            System.out.println("【输入“按社团”“日期前”“日期后”选择筛选模式】");
+        }
+
+        //用于指示循环跳出
+        boolean end = false;
+        while (!end) {
+            String input = InputStateMachine.readLine();
+            switch (filterMode) {
+                case AfterDate:
+                    for (Activity activity : machine.base.getActivities()) {
+                        if (activity.getTime().compareTo(input) >= 0) {
+                            activity.output();
+                        }
+                    }
+                    end = true;
+                    break;
+                case BeforeDate:
+                    for (Activity activity : machine.base.getActivities()) {
+                        if (activity.getTime().compareTo(input) <= 0) {
+                            activity.output();
+                        }
+                    }
+                    end = true;
+                    break;
+                case ByCorporation:
+                    Corporation corporation = machine.base.FindCorporationWithName(input);
+                    if(corporation == null){
+                        System.out.println("找不到相关社团!");
+                        System.out.println("【请输入社团名】");
+                    }else{
+                        System.out.println("【以下是"+corporation.getName()+"举办的活动】");
+                        for (Activity activity:corporation.getActivityList()) {
+                            activity.output();
+                        }
+                        end = true;
+                    }
+                    break;
+            }
+        }
+
+        System.out.println("【筛选完毕!】");
+        ShowTips();
+        //结束阻塞
+        blocked = false;
+    }
+
+    /**
+     * 显示提示
+     */
+    private void ShowTips() {
+        System.out.println();
+        System.out.println("【输入“筛选”进行筛选！】");
+        System.out.println("【输入社团或活动名查看详情！】");
+        System.out.println("【输入“进入后台”进入后台！】");
     }
 
     @Override
